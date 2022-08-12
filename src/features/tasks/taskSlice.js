2fcc64411820144fbfce7d0684fa2196663bf4ac
Jhonatan Mejia <quenandres@gmail.com>
2022-08-11 22:57:08 -0500

    Add: Funcionamiento de eliminacion con redux

--- src/features/tasks/taskSlice.js
+++ src/features/tasks/taskSlice.js
@@ -18,13 +18,19 @@ const initialState = [
 export const taskSlice = createSlice({
     name:'tasks',
     initialState,
-    reducers: { //Funcionaes
+    reducers: { //Funciones
         addTask:(state, action) => {
             state.push(action.payload);
+        },
+        deleteTask: (state, action) => {
+            const taskFound = state.find(task => task.id === action.payload);
+            if(taskFound) {
+                state.splice(state.indexOf(taskFound), 1);
+            }
         }
     }
 });
 
-export const { addTask } = taskSlice.actions;
+export const { addTask, deleteTask } = taskSlice.actions;
 
 export default taskSlice.reducer;
